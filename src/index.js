788d575e0a80626cf367845bc4d7d7dc513a55b1
Maloney L <maloneyl@users.noreply.github.com>
2018-10-03 14:29:51 +0100

    remove Hello; start Card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Hello greeting={'Hello, React Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card id='1' name='Jane Doe' email='jane@doe.com' />
+    <Card id='2' name='Mary Lee' email='mary@lee.com' />
+    <Card id='3' name='Taylor Dunn' email='taylor@dunn.com' />
+  </div>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
