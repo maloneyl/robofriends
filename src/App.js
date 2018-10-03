d9c19134ae263a6eaf26223f0ca1eac18d633ed5
Maloney L <maloneyl@users.noreply.github.com>
2018-10-03 17:32:27 +0100

    fetch robots' info instead of using hardcoded list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
   }
 
   // note: arrow <-> this
@@ -23,13 +28,17 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
 
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h3 className='tc pa3'>Loading...</h3>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      )
+    }
   }
 }
 
